272216a3921016d47996cb8fb51ad96eb6c5bc86
ss1271 <ss1271@gmail.com>
2012-05-29 16:17:42 +0100

    1st Sync

--- DateRange/DateRangeViewController.h
+++ DateRange/DateRangeViewController.h
@@ -10,4 +10,18 @@
 
 @interface DateRangeViewController : UIViewController
 
+@property (retain, nonatomic) UIButton *pickStart;
+@property (retain, nonatomic) UIButton *pickEnd;
+//@property (nonatomic, retain) UIButton *calcRange;
+//@property (nonatomic, retain) UIDatePicker *datePicker;
+@property (weak, nonatomic) IBOutlet UILabel *result;
+@property (weak, nonatomic) NSString *dateSelected;
+
+
+
+- (IBAction)showDatePicker:(UIButton *)sender;
+- (void)dueDateChanged:(UIDatePicker *)sender;
+- (IBAction)doCalculation:(id)sender;
+- (IBAction)setButtonTitleWithDate:(UIButton *)sender;
+
 @end
--- DateRange/DateRangeViewController.m
+++ DateRange/DateRangeViewController.m
@@ -7,54 +7,83 @@
 //
 
 #import "DateRangeViewController.h"
+@interface DateRangeViewController()
+@property (retain, nonatomic) id pointer;
+@property (retain, nonatomic) id actionSheetPointer;
+@end
 
 @implementation DateRangeViewController
+@synthesize result = _result;
+@synthesize pickStart = _pickStart;
+@synthesize pickEnd = _pickEnd;
+//@synthesize calcRange = _calcRange;
+//@synthesize datePicker = _datePicker;
+@synthesize dateSelected = _dateSelected;
+@synthesize pointer = _pointer;
+@synthesize actionSheetPointer = _actionSheetPointer;
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Release any cached data, images, etc that aren't in use.
+-(IBAction)showDatePicker:(UIButton *)sender {
+    //add action sheet for dismiss button
+    UIActionSheet *actionSheet = [[UIActionSheet alloc]initWithTitle:@"" delegate:self cancelButtonTitle:nil destructiveButtonTitle:nil otherButtonTitles:nil, nil];
+    
+//    _actionSheet.title = @"";
+//    _actionSheet.delegate = self;
+    
+    //Use the pointer as reference
+    _actionSheetPointer = actionSheet;
+    
+    
+    UIToolbar *pickerToolbar = [[UIToolbar alloc]initWithFrame:CGRectMake(0, 0, 320, 40)];
+    [pickerToolbar sizeToFit];
+    pickerToolbar.barStyle = UIBarStyleBlackOpaque;
+    NSMutableArray *barItems = [[NSMutableArray alloc]init];
+    UIBarButtonItem *dismissButton = [[UIBarButtonItem alloc]initWithTitle:@"Dismiss"style:UIBarButtonItemStyleDone target:self action:@selector(done_clicked:)];
+    [barItems addObject:dismissButton];
+    [pickerToolbar setItems:barItems animated:YES];
+    
+    UIDatePicker *datepicker = [[UIDatePicker alloc] init];
+//    datepicker.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+    datepicker.datePickerMode = UIDatePickerModeDate;
+    [datepicker addTarget:self action:@selector(dueDateChanged:) forControlEvents:UIControlEventValueChanged];
+//    CGSize pickerSize = [datepicker sizeThatFits:CGSizeZero];
+//    datepicker.frame = CGRectMake(0.0, 40, pickerSize.width, 0);
+    datepicker.frame = CGRectMake(0.0, 40.0, 320.0, 216.0);
+    datepicker.backgroundColor = [UIColor blackColor];
+    
+    [actionSheet addSubview:pickerToolbar];
+    [actionSheet addSubview:datepicker];
+    [actionSheet showInView:self.view];
+    [actionSheet setBounds:CGRectMake(0,0,320,480)];
+//    [datepicker release];
+    _pointer = sender;
 }
 
-#pragma mark - View lifecycle
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+-(void)dueDateChanged:(UIDatePicker *)sender {
+    NSDateFormatter* dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateStyle:NSDateFormatterLongStyle];
+    [dateFormatter setTimeStyle:NSDateFormatterNoStyle];
+    _dateSelected = [dateFormatter stringFromDate:[sender date]];
+    [self setButtonTitleWithDate:_pointer];
+//    [dateFormatter release];
 }
 
-- (void)viewDidUnload
+-(void)done_clicked:(UIActionSheet *)sender
 {
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
+    [_actionSheetPointer dismissWithClickedButtonIndex:0 animated:YES];
 }
 
-- (void)viewWillAppear:(BOOL)animated
-{
-    [super viewWillAppear:animated];
+- (IBAction)setButtonTitleWithDate:(UIButton *)sender {
+    [sender setTitle:_dateSelected forState:UIControlStateNormal];
 }
 
-- (void)viewDidAppear:(BOOL)animated
-{
-    [super viewDidAppear:animated];
+-(IBAction)doCalculation:(id)sender {
+    
 }
 
-- (void)viewWillDisappear:(BOOL)animated
-{
-	[super viewWillDisappear:animated];
+- (void)dealloc {
+//    [_pointer release];
+    
 }
 
-- (void)viewDidDisappear:(BOOL)animated
-{
-	[super viewDidDisappear:animated];
-}
-
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-    // Return YES for supported orientations
-    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
-}
 
 @end
